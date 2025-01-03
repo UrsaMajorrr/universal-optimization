c33c4755e460290bd67d96bf45fb865bc02067de
Kade Carlson <carlsonkade3232@gmail.com>
2025-01-03 11:05:04 -0500

    adjusting config file for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,38 +1,66 @@
-# Configuration file for the Sphinx documentation builder.
+import os
+import sys
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
 
-project = 'universal optimization'
+# Add the project directory to sys.path to allow Sphinx to locate the modules
+sys.path.insert(0, os.path.abspath('../../..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'universal-optimization'
 copyright = '2025, Kade Carlson'
 author = 'Kade Carlson'
 
-release = '0.1'
-version = '0.1.0'
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# -- Options for HTML output
+# List of patterns, relative to source directory, that match files directories to ignore when looking for source files.
+exclude_patterns = []
 
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'navigation_depth': 4,
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings for Google-style and NumPy-style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'special-members': '__init__',
+    'show-inheritance': True,
 }
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+autodoc_typehints = 'description'
+
